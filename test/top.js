b24abf784b32b3f1e9f1c31b87cf927e1d541b20
Kian.peymani@gmail.com <Kian.peymani@gmail.com>
2016-11-13 16:10:37 +0330

    a trivial approach to cluster join added. works good. time to re test everything and add cluster tests

--- test/top.js
+++ test/top.js
@@ -1,27 +1,27 @@
 describe('basic', function () {
   require('./tests/basic')
 })
-
-describe('data Types', function () {
-  require('./tests/dataTypes')
-})
-
-describe('transport middlewares', function () {
-  require('./tests/middleware.transport')
-})
-
-describe('service middlewares', function () {
-  require('./tests/middleware.service')
-})
-
-describe('transport authentication midlewares', function () {
-  require('./tests/auth')
-})
-
-describe('events publish and subscribe', function () {
-  require('./tests/events')
-})
-
-describe('Path Handling', function () {
-  require('./tests/path')
-})
+//
+// describe('data Types', function () {
+//   require('./tests/dataTypes')
+// })
+//
+// describe('transport middlewares', function () {
+//   require('./tests/middleware.transport')
+// })
+//
+// describe('service middlewares', function () {
+//   require('./tests/middleware.service')
+// })
+//
+// describe('transport authentication midlewares', function () {
+//   require('./tests/auth')
+// })
+//
+// describe('events publish and subscribe', function () {
+//   require('./tests/events')
+// })
+//
+// describe('Path Handling', function () {
+//   require('./tests/path')
+// })
