3826e9435b9560cd1fe96e39366c980aa7ca4d98
Kian.peymani@gmail.com <Kian.peymani@gmail.com>
2016-12-14 16:04:09 +0330

    markup for bootstraper function added. jumping to new branch

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let xyz = require('./xyz')
 let logger = require('./src/Log/Logger')
 let path = require('./src/Service/path')
+let CONFIG = require('./src/Config/config.global')
 
 module.exports = {
   xyz: xyz,
   logger: logger,
   path: path,
+  CONFIG: CONFIG,
   logUtils: require('./src/Util/ansi.colors')
 }
---
+++ src/Bootstrap/ping.default.js
@@ -0,0 +1,10 @@
+let interval = 5000
+let threshold = 2000
+let kick = 5
+
+let pingBoostraper = (xyz) => {
+  let pingInterval = setInterval(() => {
+  }, interval)
+}
+
+module.exports = pingBoostraper
