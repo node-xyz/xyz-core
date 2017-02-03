777bb14d475ae959db73aba9570c4bae2306668b
Kian.peymani@gmail.com <Kian.peymani@gmail.com>
2017-02-03 12:15:43 +0330

    test are stable now

--- test/top.js
+++ test/top.js
@@ -1,26 +1,26 @@
-// describe('basic', function () {
-//   describe('basic test as a hello world', function () {
-//     require('./tests/basic')
-//   })
-// })
-//
-// describe('data Types', function () {
-//   describe('testing different data types', function () {
-//     require('./tests/dataTypes')
-//   })
-// })
-//
-// describe('transport middlewares', function () {
-//   describe('transport middleware test', function () {
-//     require('./tests/middleware.transport')
-//   })
-// })
-//
-// describe('service middlewares', function () {
-//   describe('service layer middleware', function () {
-//     require('./tests/middleware.service')
-//   })
-// })
+describe('basic', function () {
+  describe('basic test as a hello world', function () {
+    require('./tests/basic')
+  })
+})
+
+describe('data Types', function () {
+  describe('testing different data types', function () {
+    require('./tests/dataTypes')
+  })
+})
+
+describe('transport middlewares', function () {
+  describe('transport middleware test', function () {
+    require('./tests/middleware.transport')
+  })
+})
+
+describe('service middlewares', function () {
+  describe('service layer middleware', function () {
+    require('./tests/middleware.service')
+  })
+})
 
 describe('transport authentication midlewares', function () {
   describe('simple authentication', function () {
@@ -28,14 +28,14 @@ describe('transport authentication midlewares', function () {
   })
 })
 
-// describe('cluster and cluster events', function () {
-//   describe('add and remove nodes', function () {
-//     require('./tests/events.and.cluster')
-//   })
-// })
-//
-// describe('Path Handling', function () {
-//   describe('Static path tests', function () {
-//     require('./tests/path')
-//   })
-// })
+describe('cluster and cluster events', function () {
+  describe('add and remove nodes', function () {
+    require('./tests/events.and.cluster')
+  })
+})
+
+describe('Path Handling', function () {
+  describe('Static path tests', function () {
+    require('./tests/path')
+  })
+})
