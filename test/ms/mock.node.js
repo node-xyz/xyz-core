40061c18790a4e774169500484af813fcdee8162
Kian.peymani@gmail.com <Kian.peymani@gmail.com>
2017-05-06 23:51:53 +0430

    increase test log level

--- test/ms/mock.node.js
+++ test/ms/mock.node.js
@@ -6,7 +6,7 @@ let logger
 class mockNode {
   constructor (name, port, cwd, systemConf) {
     let selfConf = {
-      'logLevel': 'verbose',
+      'logLevel': 'debug',
       'name': name,
       'host': '127.0.0.1',
       'transport': [{type: 'HTTP', port: port}]
