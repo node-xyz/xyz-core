953ca53ad3e7599a27398d8dab9702cb82c5b453
Kian.peymani@gmail.com <Kian.peymani@gmail.com>
2017-05-06 16:15:14 +0430

    remove build verison from repo

--- built/Util/machine.reporter.js
+++
@@ -1,6 +0,0 @@
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    getCPU: function () { return process.cpuUsage(); },
-    getMem: function () { return process.memoryUsage(); },
-    PID: function () { return process.pid; }
-};
